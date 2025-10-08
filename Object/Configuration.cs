9183a5428109d563cdeb1fcd5e65030ec937bd32
Sam Cartwright <samc@behrens.co.uk>
2025-10-08 14:56:28 +0100

    Add project files.

---
+++ Object/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FolderPrint.Object
+{
+    public static class Configuration
+    {
+        private static readonly string ConfigurationFile = "application.properties.json";
+        public static readonly string logPath = Path.Combine(Environment.CurrentDirectory, "FolderPrinterLogFile.txt");
+        public static string watchFolder { get; set; }
+        public static string printerName { get; set; }
+        public static string sumatraPath { get; set; }
+        public static string completedFolder { get; set; }
+        public static bool debugMode { get; set; }
+
+        public static void SetConfiguration()
+        {
+            ConfigurationBuilder builder = new();
+            IConfiguration root = builder.AddJsonFile(ConfigurationFile).Build() ?? throw new Exception($"{ConfigurationFile} Not Found");
+            watchFolder = root.GetValue<string>("watchFolder");
+            printerName = root.GetValue<string>("printerName");
+            sumatraPath = root.GetValue<string>("sumatraPath")?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"SumatraPDF.exe");
+            completedFolder = root.GetValue<string>("completedFolder");
+            debugMode = root.GetValue<bool>("debugMode");
+
+            File.AppendAllText(Path.Combine(Environment.CurrentDirectory, "logfile.txt"), $"{watchFolder},{printerName},{sumatraPath},{completedFolder}{Environment.NewLine}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿
+using FolderPrint.Object;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Configuration.SetConfiguration();
+string? defaultPrinter = GetDefaultPrinterName();
+
+if (!Directory.Exists(Configuration.watchFolder))
+{
+    File.AppendAllText(Configuration.logPath, $"Folder does not exist: {Configuration.watchFolder}");
+    return;
+}
+
+FileSystemWatcher watcher = new()
+{
+    Path = Configuration.watchFolder,
+    Filter = "*.pdf",
+    NotifyFilter = NotifyFilters.FileName | NotifyFilters.CreationTime
+};
+
+watcher.Created += OnNewPdf;
+watcher.EnableRaisingEvents = true;
+
+while (true)
+{
+   Thread.Sleep(1000);
+}
+
+static void OnNewPdf(object sender, FileSystemEventArgs e)
+{
+
+    while (!IsFileReady(e.FullPath))
+    {
+        Thread.Sleep(500);
+    }
+
+    string? defaultPrinter = GetDefaultPrinterName();
+    if (Configuration.debugMode)
+    {
+        File.AppendAllText(Configuration.logPath, $"DEBUGMODE @ [{DateTime.Now:F}] Defualt Printer Name => \"{defaultPrinter}\"{Environment.NewLine}");
+
+        foreach (string printer in PrinterSettings.InstalledPrinters)
+        {
+            File.AppendAllText(Configuration.logPath, $"DEBUGMODE @ [{DateTime.Now:F}] Found Printer Named => \"{printer}\"{Environment.NewLine}");
+        }
+        //no need for this to constantly spill out into a file each time
+        Configuration.debugMode = false;
+    }
+    try
+    {
+        ProcessStartInfo psi = new()
+        {
+            FileName = Configuration.sumatraPath,
+            Arguments = $"-print-to \"{Configuration.printerName ?? defaultPrinter}\" \"{e.FullPath}\"",
+            CreateNoWindow = true,
+            WindowStyle = ProcessWindowStyle.Hidden
+        };
+        Process.Start(psi);
+        try
+        {
+            File.Move($"{Path.Combine(Configuration.watchFolder, Path.GetFileName(e.FullPath))}", $"{Path.Combine(Configuration.completedFolder, Path.GetFileName(e.FullPath))}");
+        }
+        catch
+        {
+            File.AppendAllText(Configuration.logPath, $"ERROR @ [{DateTime.Now:F}] Failed to Move: {Path.Combine(Configuration.watchFolder, Path.GetFileName(e.FullPath))}{Environment.NewLine}");
+        }
+    }
+    catch (Exception ex)
+    {
+        File.AppendAllText(Configuration.logPath, $"ERROR @ [{DateTime.Now:F}] Failed to print: {ex.Message}{Environment.NewLine}");
+    }
+}
+[DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+static extern bool GetDefaultPrinter(StringBuilder pszBuffer, ref int pcchBuffer);
+
+static string? GetDefaultPrinterName()
+{
+    int pcchBuffer = 256;
+    StringBuilder buffer = new(pcchBuffer);
+    if (GetDefaultPrinter(buffer, ref pcchBuffer))
+    {
+        return buffer.ToString();
+    }
+    else
+    {
+        return null;
+    }
+}
+static bool IsFileReady(string path)
+{
+    try
+    {
+        using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.None))
+        {
+            return true;
+        }
+    }
+    catch
+    {
+        return false;
+    }
+}
